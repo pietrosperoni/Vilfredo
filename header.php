68e85cc9dcc9799e59dda5f789756910437712f7
Derek Paterson <vilfredo@gmx.com>
2009-11-03 12:52:12 +0000

    Added GPL License Agreement information. License and header to header.php.

--- header.php
+++ header.php
@@ -1,4 +1,29 @@
 <?php
+/**
+*  Software License Agreement (Affero GPL)
+*  
+*  Vilfredo
+*  You can use this system to make decisions amongst groups of people.
+*  Vilfredo is used to really explore the possible alternative answers to an open question, 
+*  and find an answer endorsed by as many people as possible (theorethically everybody). 
+*  As such the question should be an open question. 
+*  The Vilfredo project is at http://www.vilfredo.org
+*  Copyright (C) 2009  Vilfredo.org
+*  
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as
+*  published by the Free Software Foundation, either version 3 of the
+*  License, or (at your option) any later version.
+*  
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*  
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*  
+*/
 ob_start();
 ?>
 <html>
