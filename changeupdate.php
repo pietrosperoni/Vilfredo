2f3e561299bc8bcb4a4bd7690d9ac24131ca96e6
Pietro Speroni di Fenizio <mercurial@pietrosperoni.it>
2010-01-05 18:20:09 +0100

    security issue. The system checks that you were in the room of a question before letting you subscribe to that.

--- changeupdate.php
+++ changeupdate.php
@@ -7,23 +7,28 @@ if ($userid)
 {
 	$question = $_POST['question'];
 	$room = $_POST['room'];
-	$urlquery = CreateQuestionURL($question, $room);
-
-	$sql = "SELECT * FROM updates WHERE question = ".$question." AND  user = ".$userid." LIMIT 1 ";
-	$response = mysql_query($sql);
-	$row = mysql_fetch_array($response);
-
-	if ($row)
+	
+	
+	if ($room==GetQuestionRoom($question))
 	{
+	
+		$urlquery = CreateQuestionURL($question, $room);
 
-		$sql = "DELETE FROM updates WHERE  updates.question = " . $question . " AND updates.user = " . $userid . "  ";
-		if (!mysql_query($sql)) error("Database update failed");
-		#mysql_query($sql);
-	}else{
-		$how="asap";
-		$sql = 'INSERT INTO `updates` (`user`, `question`, `how`) VALUES (\'' . $userid . '\', \'' . $question . '\', \'' . $how . '\');';
-		if (!mysql_query($sql)) error("Database update failed");
-		#mysql_query($sql);
+		$sql = "SELECT * FROM updates WHERE question = ".$question." AND  user = ".$userid." LIMIT 1 ";
+		$response = mysql_query($sql);
+		$row = mysql_fetch_array($response);
+
+		if ($row)
+		{
+			$sql = "DELETE FROM updates WHERE  updates.question = " . $question . " AND updates.user = " . $userid . "  ";
+			if (!mysql_query($sql)) error("Database update failed");
+			#mysql_query($sql);
+		}else{
+			$how="asap";
+			$sql = 'INSERT INTO `updates` (`user`, `question`, `how`) VALUES (\'' . $userid . '\', \'' . $question . '\', \'' . $how . '\');';
+			if (!mysql_query($sql)) error("Database update failed");
+			#mysql_query($sql);
+		}
 	}
 	header("Location: viewquestion.php".$urlquery);
 }
