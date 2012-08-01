24d88e43de011a457b79c2e638762219675e0ae9
Derek Paterson <derekpa>
2012-08-01 16:05:14 +0100

    Sample plugin file using plugin

---
+++ 99percent.php
@@ -0,0 +1,12 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>We are the 99%</title>
+</head>
+<body>
+
+<h3>Pietro's Bubbles Portal</h3>
+
+<iframe id="bubbleframe" src="http://vilfredo.org/bubbles/bubbleboxpluginfull.php?qb=5" width="850" height="600" frameborder="0" ></iframe>
+
+</body>
+</html>
