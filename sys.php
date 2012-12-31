9d7f2a03a784a35df84eddefd19f8ab26b137acc
Derek Paterson <derek@vilfredo.org>
2012-12-31 14:42:37 +0000

    Updated sys.php

--- sys.php
+++ sys.php
@@ -1,18 +1,39 @@
 <?php
 
-define("PWD_HASH_PREFIX", "bde5ffc362ece6a380");
-define("ID_SALT", "bde5ffc362ece6a380");
-//require "sys2.php";
+define("PWD_HASH_PREFIX", ""); #define the prefix for the password autentication
+define("ID_SALT", ""); #define the salt for the password autentication
+
+// NEW *********
+if (!function_exists('checkUserPassword')) 
+{
+	function checkUserPassword($userid, $password, $dbhash)
+	{
+		//set_log(__FUNCTION__." in sys.php called....");
+		// Check against hash
+		if (encryptPWD($password) == $dbhash)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	};
+}
+
+if (!function_exists('encryptUserPassword')) 
+{
+	function encryptUserPassword($password)
+	{
+		//set_log(__FUNCTION__." in sys.php called....");
+		return encryptPWD($password);
+	};
+}
+//**************
 
 function encryptPWD3($password, $prefix=PWD_HASH_PREFIX)
 {
 	return hash('sha256', $prefix . $password);
-}
-
-function createEmailVerificationKey($username, $email)
-{
-	$key = $username . $email  . date('mY');
-	return md5($key);
 }
 
 function encryptPWD2($password, $prefix=PWD_HASH_PREFIX)
@@ -62,34 +83,6 @@ if (!function_exists('encryptUserPassword'))
 	};
 }
 //**************
-function generateRandomString($length) 
-{
-	$random = '';
-	for ($i = 0; $i < $length; $i++) 
-	{
-		$random .= chr(rand(ord('a'), ord('z')));
-	}
-	return $random;
-}
-
-// Deleted yet used for pwd recovery
-function gen_uuid($len=10)
-{
-    $hex = md5("my_cake_on_fire_25479" . uniqid("", true));
-
-    $pack = pack('H*', $hex);
-
-    $uid = base64_encode($pack);        // max 22 chars
-
-    $uid = ereg_replace("[^A-Za-z0-9]", "", $uid);    // mixed case
-    //$uid = ereg_replace("[^A-Z0-9]", "", strtoupper($uid));    // uppercase only
-
-    while (strlen($uid)<$len)
-        $uid = $uid . gen_uuid(22);     // append until length achieved
-
-    return substr($uid, 0, $len);
-}
-
 
 function generateIDENTIFIER($userid, $salt = ID_SALT)
 {
