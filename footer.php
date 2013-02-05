3620986499cf6d239f9074fe9fc97295bd3d33a1
Pietro Speroni di Fenizio <pietrosperoni@yahoo.com>
2013-02-05 14:02:11 +0000

    Added link to twitter account for feedback

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
 			</div>
 			<div id="footer">
-				<p><em><?=$VGA_CONTENT['madeby_txt']?> <a href="http://pietrosperoni.it/">Pietro</a> &amp; Derek.</em></p>
+				<p><em><?=$VGA_CONTENT['madeby_txt']?> <a href="http://pietrosperoni.it/">Pietro</a> &amp; Derek. For bugs and suggestions please contact Pietro on twitter: <a href="http://twitter.com/pietrosperoni">@pietrosperoni</a></em></p>
 			</div>
 			
 			<?php echo facebook_fbconnect_init_js(DISPLAY_FACEBOOK_LOGIN); ?>
