7fb2f4f834e01296ebfd59d83df261a7e18272c6
Derek Paterson <athens_code@gmx.com>
2013-10-31 11:09:14 +0000

    Add DB updates for comments, Q&A and likes

---
+++ sql/update_comments_add_likes_table.sql
@@ -0,0 +1,15 @@
+ALTER TABLE `comments` CHANGE `type` `type` ENUM( 'confused', 'dislike', 'support', 'question', 'answer' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL 
+
+ALTER TABLE `comments` ADD `replyto` INT( 11 ) NOT NULL DEFAULT '0';
+
+CREATE TABLE IF NOT EXISTS `user_comment_likes` (
+  `userid` int(11) NOT NULL,
+  `commentid` int(11) NOT NULL,
+  `proposalid` int(11) NOT NULL,
+  PRIMARY KEY (`userid`,`commentid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT IGNORE INTO `user_comment_likes`(`userid`, `commentid`, `proposalid`)
+SELECT `userid`, `commentid`, `proposalid`
+FROM `oppose`
+WHERE commentid != 0;
